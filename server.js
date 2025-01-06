adbc19702278c241ab1246d84df494c5354ce9cc
jin_neuron <yuujinyuujin77@gmail.com>
2025-01-06 19:57:26 +0900

    play audio responce

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const port = 3000;
 app.use(express.static("public"));
 // WebSocketサーバーを作成
 const wss = new WebSocket.Server({ noServer: true });
+const config = fs.readFileSync("./settingPrompt.txt", 'utf8');
+let isOrdering = false;
 
-var config = fs.readFileSync("./settingPrompt.txt", 'utf8');
 // WebSocket接続の処理
 wss.on("connection", (clientWs) => {
     console.log("Client connected");
 
-    const url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01";
+    const url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17";
     const openAiWS = new WebSocket(url, {
         headers: {
             "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
@@ -28,49 +29,74 @@ wss.on("connection", (clientWs) => {
     // 接続完了時に呼ばれる
     openAiWS.on("open", function open() {
         console.log("openAI WS connected");
-        //注文仕様へ設定
-        const event = {
-            type: 'conversation.item.create',
-            item: {
-                type: 'message',
-                role: 'user',
-                content: [{
-                    type: 'input_text',
-                    text: config.toString(),
-                }]
+
+        openAiWS.send(JSON.stringify({
+            type: 'session.update',
+            session: {
+                voice: 'sage',
+                input_audio_transcription: { model: 'whisper-1' },
+                turn_detection: { type: "server_vad" }
             }
-        };
-        openAiWS.send(JSON.stringify(event));
-        openAiWS.send(JSON.stringify({type: 'response.create'}));
+        }));
     });
 
     // クライアントからメッセージを受信したとき
-    clientWs.on("message", (message) => {
-        console.log("Message from client:", message.toString());
-        const event = {
-            type: 'conversation.item.create',
-            item: {
-                type: 'message',
-                role: 'user',
-                content: [{
-                    type: 'input_text',
-                    text: message.toString(),
-                }]
-            }
-        };
-        openAiWS.send(JSON.stringify(event));
-        openAiWS.send(JSON.stringify({type: 'response.create'}));
+    clientWs.on("message", (data) => {
+        const serverEvent = JSON.parse(data);
+
+        if(serverEvent.type === "sendChat" && isOrdering){
+            console.log("Message from client:", serverEvent.text);
+            
+            const event = {
+                type: 'conversation.item.create',
+                    item: {
+                    type: 'message',
+                    role: 'user',
+                    content: [{
+                        type: 'input_text',
+                        text: serverEvent.text,
+                    }]
+                }
+            };
+            openAiWS.send(JSON.stringify(event));
+            openAiWS.send(JSON.stringify({type: 'response.create'}));
+        }else if (serverEvent.type === "startOrder" && !isOrdering){
+            isOrdering = true;
+            //注文仕様へ設定
+            const event = {
+                type: 'conversation.item.create',
+                item: {
+                    type: 'message',
+                    role: 'user',
+                    content: [{
+                        type: 'input_text',
+                        text: config.toString(),
+                    }]
+                }
+            };
+
+            openAiWS.send(JSON.stringify(event));
+            openAiWS.send(JSON.stringify({type: 'response.create'}));
+        }
     });
 
      // OpenAIからデータを受信したとき
     openAiWS.on("message", (data) => {
         const serverEvent = JSON.parse(data);
 
+        if (serverEvent.type === "response.audio.delta") {
+             // 部分的な応答をリアルタイムで再生
+            clientWs.send(JSON.stringify({
+                type : "delta.audio",
+                text : serverEvent.delta,
+            }));
+            // Access Base64-encoded audio chunks
+        }
         //console.log(serverEvent)
         if (serverEvent.type === "response.audio_transcript.delta") {
             // 部分的な応答をリアルタイムで表示
             clientWs.send(JSON.stringify({
-                type : "delta",
+                type : "delta.text",
                 text : serverEvent.delta,
             }));
         }
